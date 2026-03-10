c2e1ab27f61e16c5510640dba0273ff527d2959f
Kreyno93 <michael.wagener@neuefische.de>
2026-03-10 11:21:57 +0100

    adding basic and alternative method with module

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-# Prime Number Checker
-# 1. loop for printing numbers between 1 and 250
+from primePy import primes
 
-number = 1
-while number <= 250:
-    print(number)
-    number = number + 1
+# Prime number checker easiest method, counting from 1 to 250
+
+
+def is_prime_basic(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def alternative_prime_checker_with_module():
+    # Prime number checker using primePy library
+    for number in range(1, 251):
+        if primes.check(number):
+            print(f"{number} is a prime number.")
+
+
+if __name__ == "__main__":
+    print("Prime numbers from 1 to 250 using the basic method:")
+    for number in range(1, 251):
+        if is_prime_basic(number):
+            print(f"{number} is a prime number.")
+
+    print("\nPrime numbers from 1 to 250 using the primePy library:")
+    alternative_prime_checker_with_module()
